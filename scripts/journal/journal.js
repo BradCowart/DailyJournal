c044a998490244cbc9eb460c24be40c33ac5208d
BradCowart <Brad@TierStrategies.com>
2020-06-09 11:04:30 -0500

    Define Data

---
+++ scripts/journal/journal.js
@@ -0,0 +1,17 @@
+const journalEntry = {
+    date:"6/1/2020",
+    concept:"Concept 1",
+    Entry:"Entry 1",
+    mood:"Happy",
+
+    date:"6/2/2020",
+    concept:"Concept 2",
+    Entry:"Entry 2",
+    mood:"Tired",
+
+    date:"6/3/2020",
+    concept:"Concept 3",
+    Entry:"Entry 3",
+    mood:"Happy"
+
+}
