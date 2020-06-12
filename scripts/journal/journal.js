008534add4f4343062f03410b30ccc75407f3d3f
BradCowart <Brad@TierStrategies.com>
2020-06-11 20:40:59 -0500

    Created dynamic list of journal entries

--- scripts/journal/journal.js
+++ scripts/journal/journal.js
@@ -1,17 +1,46 @@
-const journalEntry = {
+
+const journalEntries =[ 
+{
     date:"6/1/2020",
     concept:"Concept 1",
     Entry:"Entry 1",
-    mood:"Happy",
-
+    mood:"Happy" 
+},
+{
     date:"6/2/2020",
     concept:"Concept 2",
     Entry:"Entry 2",
     mood:"Tired",
-
+},
+{
     date:"6/3/2020",
     concept:"Concept 3",
     Entry:"Entry 3",
     mood:"Happy"
-
 }
+]
+
+const journalEntryConverter = (journalEntryObject)=>{
+    const journalHTMLReresentation =`
+        <section>Entries
+        <div>${journalEntryObject.date}</div>
+        <div>${journalEntryObject.concept}</div>
+        <div>${journalEntryObject.Entry}</div>
+        <div>${journalEntryObject.mood}</div>
+        <br><br>
+        </section>
+        `
+        return journalHTMLReresentation
+    
+}
+
+const JournalEntryList = (allEntries) =>{
+    for (const entry of allEntries ){
+    const journalHTML = journalEntryConverter(entry)
+    const journalEntryArticleElement= document.querySelector(".journalEntries")
+    journalEntryArticleElement.innerHTML += journalHTML
+   // console.log(entry)
+}
+}
+
+JournalEntryList(journalEntries)
